031b6f2bd4088070ae5ad0138cc81188bd0e0404
kingofmess <151908469+kingofmess@users.noreply.github.com>
2024-05-24 13:15:57 +0900

    Develop v1.05 Explosion Particle
    - Explosion Particle
    
    - After Fix = When Collision Enemy,
     i have to Change SetOff Time,
    now Hard Coding....

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,24 +1,48 @@
+using System.Collections;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
     public float moveSpeed;
+    public GameObject go_bullet;
+    public GameObject explosionParticle;
 
-    // Update is called once per frame
     void Update()
     {
-        float dirY = moveSpeed * Time.deltaTime;
-        Vector3 dir = new Vector2(0, dirY);
-        transform.Translate(dir);
+        if(this.gameObject.activeSelf)
+        {
+            float dirY = moveSpeed * Time.deltaTime;
+            Vector3 dir = new Vector2(0, dirY);
+            transform.Translate(dir);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
+            explosionParticle.SetActive(true);
+
+            go_bullet.SetActive(false);
+
             Destroy(collision.gameObject);
 
-            Destroy(this.gameObject);
+            // 파괴 애니메이션 끝나고 사라져야 하는데 안되서 일단 2로 하드코딩
+            Invoke("SetOffBullet", 2f);
         }
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            SetOffBullet();
+        }
+    }
+
+    private void SetOffBullet()
+    {
+        this.gameObject.SetActive(false);
     }
 }
